8b84bdf878b7024c3574f90acc4f7e180c226334
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-17 23:14:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,41 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
+  get '/' do
     'Hello World'
   end
-  
+
   get '/posts/new' do
     erb :new
   end
-  
+
   post '/posts' do
     @post = Post.create(params)
     redirect '/posts'
   end
-  
+
   get '/posts' do
     @posts = Post.all
     erb :index
   end
-  
+
   get '/posts/:id' do
     @post = Post.find(params[:id])
     erb :show
   end
-  
+
   get '/posts/:id/edit' do
     @post = Post.find(params[:id])
     erb :edit
   end
-  
+
   patch '/posts/:id' do
+    binding.pry
     @post = Post.find(params[:id])
     @post.update(:name => params[:name], :content => params[:content])
     @post.save
     redirect_to '/posts/:id'
   end
-  
+
   delete '/posts/:id/delete' do
     @deleted_post_name = ""
     @post = Post.find(params[:id])
